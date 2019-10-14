e84c51c527b14b91d1180717da6781155917e976
Nik <41297691+CyberEssence@users.noreply.github.com>
2019-10-14 19:35:46 +0400

    added RadioButton menu in layout and code

--- app/src/main/java/com/cyberessence/cyberorangeteam/gamewithoutfragments/Game.java
+++ app/src/main/java/com/cyberessence/cyberorangeteam/gamewithoutfragments/Game.java
@@ -11,6 +11,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -44,6 +46,7 @@ public class Game extends AppCompatActivity implements GameFragment.onSomeEventL
     MediaPlayer mpDrawWrong;
 
     Button buttonSelectMode;
+    int previousSelectedMode = 0;
 
     public void hideNavigator(){
         View decorView =getWindow().getDecorView();
@@ -90,6 +93,16 @@ public class Game extends AppCompatActivity implements GameFragment.onSomeEventL
                 finish();
             }
         });
+
+        buttonSelectMode = (Button) findViewById(R.id.buttonSelectMode);
+
+        buttonSelectMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerForContextMenu(v);
+                openContextMenu(v);
+            }
+        });
    }
 
     @Override
@@ -100,5 +113,41 @@ public class Game extends AppCompatActivity implements GameFragment.onSomeEventL
             else gameFragment.setDataForQuestions(DataForQuestion.dataForQuestionsFood);
 
 
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem itemGame = menu.findItem(R.id.itemGame);
+        MenuItem itemRandomGame = menu.findItem(R.id.itemRandomGame);
+
+        if (previousSelectedMode==1)
+        {
+            itemGame.setChecked(true);
+        }
+        else if (previousSelectedMode==2)
+        {
+            itemRandomGame.setChecked(true);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.itemGame)
+        {
+            previousSelectedMode = 1;
+            Toast.makeText(getBaseContext(),"Обычный режим",Toast.LENGTH_LONG).show();
+        }
+        else if (item.getItemId()==R.id.itemRandomGame)
+        {
+            previousSelectedMode = 2;
+            Toast.makeText(getBaseContext(),"Случайные карточки", Toast.LENGTH_LONG).show();
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
